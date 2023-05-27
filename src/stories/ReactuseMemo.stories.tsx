65d9bb9f4c12faf11deeeb9e08fc9bdfb34bc079
Aleksey <easy.mail.to.copy@gmail.com>
2023-05-27 22:28:59 +0300

    Created and learned useMemo in stories

---
+++ src/stories/ReactuseMemo.stories.tsx
@@ -0,0 +1,76 @@
+import React, {useMemo, useState} from "react";
+
+export default {
+    title: "React useMemo",
+}
+
+export const ReactuseMemo = () => {
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        let tempResulA = 1;
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 10000000) {
+                fake++;
+                const fakeValue = Math.random();
+            }
+            tempResulA *= i
+
+        }
+        return tempResulA
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return <>
+        <input type="text" value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+        <input type="text" value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>
+            {resultA}
+        </div>
+        <div>
+            {resultB}
+        </div>
+
+
+    </>
+}
+
+const Users = (props: { users: Array<string> }) => {
+    console.log("Users appears")
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const UsersHOC = React.memo(Users)
+
+export const ReactuseMemoWithReactMemo = () => {
+    console.log("ReactuseMemoWithReactMemo")
+    const [counter, setCounter] = useState<number>(0)
+    const [users, setUsers] = useState<Array<string>>(["Kaya", "Katya", "Karina"])
+
+    const filteredUsers = useMemo(() => {
+       return  users.filter(u => u.toLowerCase().indexOf("y") > -1)
+    }, [users])
+
+    const addUser = () => {
+        setUsers([...users, "Mariya"])
+    }
+    return <>
+
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>+</button>
+        {counter}
+        <UsersHOC users={filteredUsers}/>
+    </>
+}
